e0784fd2b6f79b8cee8caccca47d95a1235330ea
mikalaki <mikalakis@hotmail.gr>
2019-12-19 22:14:28 +0200

    Sequential ready,as reported passed online tester!

--- src/ising_sequential.c
+++ src/ising_sequential.c
@@ -2,14 +2,11 @@
 #include "stdlib.h"
 
 
-void step(int * G, double * w , int n);
-void evolve(int * Lat, double * weights , int n, int a , int b);
+//Functions Declaration
+void step(int **Gptr,int **newMat, double * w , int n);
+void getTheSpin(int * Lat,int * newLat, double * weights , int n, int a , int b);
+void pointer_swap(int **a , int **b);
 
-// static double weights[25]={ 0.004,  0.016,  0.026,  0.016,   0.004,
-//                             0.016,  0.071,  0.117,  0.071,   0.016,
-//                             0.026,  0.117,    0  ,  0.117,   0.026,
-//                             0.016,  0.071,  0.117,  0.071,   0.016,
-//                             0.004,  0.016,  0.026,  0.016,   0.004};
 //! Ising model evolution
 /*!
 
@@ -25,46 +22,47 @@ void evolve(int * Lat, double * weights , int n, int a , int b);
 
 
 void ising( int *G, double *w, int k, int n){
-  // w= (double)malloc(25*sizeof(double));
-  // w={ 0.004,  0.016,  0.026,  0.016,   0.004,
-  //                             0.016,  0.071,  0.117,  0.071,   0.016,
-  //                             0.026,  0.117,    0  ,  0.117,   0.026,
-  //                             0.016,  0.071,  0.117,  0.071,   0.016,
-  //                             0.004,  0.016,  0.026,  0.016,   0.004};
+
+  //The second Matrix We use
+  int * newG= (int *)malloc(sizeof(int)*n*n);
 
   //Evolving the model for k steps
   for(int i=0 ; i<k ;i++){
-    step(G,w,n);
+    step(&G,&newG,w,n);
   }
+
+  //Getting the right values to the initial Lattice matrix for odd number of spots
+  //Freeing memory of the second matrix from the heap to avoid memory leaks.
+  if((k%2)!=0){
+    memcpy (newG, G, sizeof(int)*n*n);
+    free(G);
+  }
+  else
+    free(newG);
+
+
+
 
 
 }
 
-void step(int * G, double * w , int n){
+void step(int **Gptr,int **newMat, double * w , int n){
 
-  // /*We will use step equals to 3 in the for loops which scan the G matrix(lattice)
-  //   in order sequential controls aren't
-  //   dependent on immediate previous-ones (we scan spins by 3 ex. if we check the spin
-  //   in G[1][1] in one iteration, in the next iteration we will check the spin in
-  //   G[4][4], which hasn't G[1][1] in its "weight window").In order to scan all the
-  //   spins of the lattice we use offsets.
-  //   */
-
-  for(int c_offset =0 ; c_offset<3 ; c_offset++ ){
-    for (int r_offset = 0; r_offset <3; r_offset++) {
-      //scanning the Lattice
-      for(int i=c_offset; i<n; i+=3){
-        for(int j=r_offset; j<n; j+=3){
-          evolve(G,w,n,i,j);
-        }
-      }
+  //scanning the Lattice
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      getTheSpin(*Gptr,*newMat,w,n,i,j);
     }
   }
 
+  //Swapping the pointers between the two Matrices
+  pointer_swap(Gptr,newMat);
+
+
 
 }
 
-void evolve(int * Lat, double * weights , int n, int a , int b){
+void getTheSpin(int * Lat,int * newLat, double * weights , int n, int a , int b){
   double total=0;
   int idxR,idxC;
   //Getting the total for a certain spin.
@@ -72,6 +70,9 @@ void evolve(int * Lat, double * weights , int n, int a , int b){
     for(int j=b-2;j<b+3;j++ ){
       if((i==a) && (j==b))
         continue;
+
+      //using modulus arithmetic for handle the edges
+      //Getting the modulus from the remainder in negative values of Cmodulus operator
       idxR= ((i%n)<0 )? (i%n +n) : i%n;
       idxC= ((j%n)<0 )? (j%n +n) : j%n;
 
@@ -79,19 +80,22 @@ void evolve(int * Lat, double * weights , int n, int a , int b){
     }
   }
 
-  double dE=2*Lat[a*n+b]*total;
-  if(dE <= 0){
-    Lat[a*n+b]=Lat[a*n+b]*(-1);
+  //Checking the conditions
+  //if (total ==0), with taking into account possible floating point errors
+  if( (total<1e-6)  &&  (total>(-1e-6)) ){
+    newLat[a*n+b]=Lat[a*n+b];
   }
-     // for i in range(n-1, n+2):
-   //     for j in range(m-1, m+2):
+  else if(total<0){
+    newLat[a*n+b]=-1;
+  }
+  else if(total>0){
+    newLat[a*n+b]=1;
+  }
 
-   //         if i == n and j == m:
-   //             continue
-   //         total += field[i % N, j % M]
-   // dE = 2 * field[n, m] * total
-   // if dE <= 0:
-   //     field[n, m] *= -1
-   // elif np.exp(-dE * beta) > np.random.rand():
-   //     field[n, m] *= -1
+}
+
+void pointer_swap(int **a , int **b){
+  int * temp=*a;
+  *a=*b;
+  *b=temp;
 }
